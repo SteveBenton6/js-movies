b0b4c1429a805c39f7f3e8ff4da32513ba0f80af
Steve Benton <steve@bentonemail.uk>
2026-06-12 17:46:05 +0100

    add input field, detect in JS and fetch from API

--- app.js
+++ app.js
@@ -6,14 +6,19 @@ const apiKey = API_KEY;
 
 console.log(apiKey);
 
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: apiKey,
-      i: "tt0848228",
+      s: searchTerm,
     },
   });
   console.log(response.data);
 };
 
 fetchData();
+
+const input = document.querySelector("input");
+input.addEventListener("input", (event) => {
+  fetchData(event.target.value);
+});
