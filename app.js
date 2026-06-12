3ddff98075616afc734664ec9ba94b73307e60a1
Steve Benton <steve@bentonemail.uk>
2026-06-12 14:50:11 +0100

    add initial api fetch

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ console.log("Hi there!");
 const apiKey = API_KEY;
 
 console.log(apiKey);
+
+const fetchData = async () => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: apiKey,
+      s: "avengers",
+    },
+  });
+  console.log(response.data);
+};
+
+fetchData();
