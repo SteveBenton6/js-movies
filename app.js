81d8519d31882cfcf2c05bfd10dea1280410b7c0
Steve Benton <steve@bentonemail.uk>
2026-06-19 16:54:19 +0100

    remove movie references in automcomplete

--- app.js
+++ app.js
@@ -6,24 +6,8 @@ const apiKey = API_KEY;
 
 console.log(apiKey);
 
-const fetchData = async (searchTerm) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
-    params: {
-      apikey: apiKey,
-      s: searchTerm,
-    },
-  });
-
-  if (response.data.Error) {
-    return [];
-  }
-
-  return response.data.Search;
-};
-
 createAutoComplete({
   root: document.querySelector(".autocomplete"),
-  fetchData,
   renderOption(movie) {
     const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
     return `
@@ -36,6 +20,18 @@ createAutoComplete({
   },
   inputValue(movie) {
     return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: apiKey,
+        s: searchTerm,
+      },
+    });
+    if (response.data.Error) {
+      return [];
+    }
+    return response.data.Search;
   },
 });
 
